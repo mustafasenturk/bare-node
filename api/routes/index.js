79acba1bfa5ed604c061d814eb750002e1239f42
mustafasenturk <mustafayasinsenturk@icloud.com>
2024-11-16 19:51:24 +0300

    db models are initialized

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const config = require('../config');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'ms' });
+  res.render('index', { title: 'ms', config });
 });
 
 module.exports = router;
