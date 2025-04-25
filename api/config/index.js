f6f964f47006d44eedf10c99221b26cb1dbb4afe
Yasin <73742526+mustafasenturk@users.noreply.github.com>
2025-04-25 18:34:52 +0300

    Delete api directory

--- api/config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  PORT: process.env.PORT || 3000,
-  LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
-  CONNECTION_STRING:
-    process.env.CONNECTION_STRING || 'mongodb://localhost:27017/ms',
-};
